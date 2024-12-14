c3f8619077002d8dd849869ea383d8851c7916e2
Danio <daniofioredev@gmail.com>
2024-12-14 20:22:58 +0100

    formatted the execution of the query code by moving it in a separate function

--- app/database/db.php
+++ app/database/db.php
@@ -1,6 +1,32 @@
 <?php
 require('connect.php');
 
+function executeQuery($sql, $data) {
+  global $conn;
+
+  $statement = $conn->prepare($sql);
+  // transform the $data array into a list of values
+  $values = array_values($data);
+  // determine the type of the values, is not mandatory but it's a good practice
+  $types = '';
+  foreach ($values as $value) {
+    if (is_int($value)) {
+      $types .= 'i'; // integer
+    } else if (is_double($value)) {
+      $types .= 'd'; // double
+    } else if (is_string($value)) {
+      $types .= 's'; // string
+    } else {
+      $types .= 'b'; // blob and other types
+    }
+  }
+  // bind the values to the statement
+  $statement->bind_param($types, ...$values);
+  $statement->execute();
+
+  return $statement;
+}
+
 function selectAll($table_name, $conditions = []) {
   // if you want to use the $conn variable inside a function, you have to use the global keyword
   global $conn;
@@ -21,25 +47,7 @@ function selectAll($table_name, $conditions = []) {
     }
   }
 
-  $statement = $conn->prepare($sql);
-  // transform the $conditions array into a list of values
-  $values = array_values($conditions);
-  // determine the type of the values, is not mandatory but it's a good practice
-  $types = '';
-  foreach ($values as $value) {
-    if (is_int($value)) {
-      $types .= 'i'; // integer
-    } else if (is_double($value)) {
-      $types .= 'd'; // double
-    } else if (is_string($value)) {
-      $types .= 's'; // string
-    } else {
-      $types .= 'b'; // blob and other types
-    }
-  }
-  // bind the values to the statement
-  $statement->bind_param($types, ...$values);
-  $statement->execute();
+  $statement = executeQuery($sql, $conditions);
   $records = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
 
   return $records;
@@ -65,26 +73,9 @@ function selectOne($table_name, $conditions = []) {
     }
   }
 
-  $slq .= " LIMIT 1";
-  $statement = $conn->prepare($sql);
-  // transform the $conditions array into a list of values
-  $values = array_values($conditions);
-  // determine the type of the values, is not mandatory but it's a good practice
-  $types = '';
-  foreach ($values as $value) {
-    if (is_int($value)) {
-      $types .= 'i'; // integer
-    } else if (is_double($value)) {
-      $types .= 'd'; // double
-    } else if (is_string($value)) {
-      $types .= 's'; // string
-    } else {
-      $types .= 'b'; // blob and other types
-    }
-  }
-  // bind the values to the statement
-  $statement->bind_param($types, ...$values);
-  $statement->execute();
+  $sql .= " LIMIT 1";
+
+  $statement = executeQuery($sql, $conditions);
   $records = $statement->get_result()->fetch_assoc();
 
   return $records;
