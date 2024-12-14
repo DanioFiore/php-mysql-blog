9457081a040670f5cae301f3afab5566e4563c73
Danio <daniofioredev@gmail.com>
2024-12-14 22:08:14 +0100

    created helpers and optimized user registration

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,15 +1,32 @@
 <?php
 include(ROOT_PATH . "/app/database/db.php");
 include(ROOT_PATH . "/utilities.php");
+include(ROOT_PATH . "/app/helpers/validators/validateUser.php");
+
+// create variables to return the data from the form when validaiton fails, so the user can see the data that they entered
+$username = '';
+$email = '';
+$password = '';
+$passwordConf = '';
 
 // from the front-end form of register.php we receive the data from the form based on the name attribute of the input fields, so here if we have a $_POST with register-btn key, we know that the form was submitted from register-btn
 if (isset($_POST['register-btn'])) {
-  // we unset the register-btn and passwordConf keys from the $_POST array and add admin to save only the data that we want to insert into the database
-  unset($_POST['register-btn'], $_POST['passwordConf']);
-  $_POST['admin'] = 0;
-  $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-  $user_id = create('users', $_POST);
-  $user = selectOne('users', ['id' => $user_id]);
+  // validation
+  $errors = validateUser($_POST);
+  
+  if (empty($errors)) {
+    // we unset the register-btn and passwordConf keys from the $_POST array and add admin to save only the data that we want to insert into the database
+    unset($_POST['register-btn'], $_POST['passwordConf']);
+    $_POST['admin'] = 0;
+    $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  
+    $user_id = create('users', $_POST);
+    $user = selectOne('users', ['id' => $user_id]);
+  } else {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordConf = $_POST['passwordConf'];
+  }
 }
 ?>
---
+++ app/helpers/formErrors.php
@@ -0,0 +1,9 @@
+<?php if (!empty($errors)): ?>
+  <div class="msg error">
+    <?php foreach($errors as $error): ?>
+      <li>
+        <?php echo $error ?>
+      </li>
+    <?php endforeach; ?>
+  </div>
+<?php endif; ?>
