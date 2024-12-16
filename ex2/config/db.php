8fb2d9110f208e1f8c285a100a03b8b3d7336324
Danio <daniofioredev@gmail.com>
2024-12-16 20:01:46 +0100

    splitted project in two folders and started new

---
+++ ex2/config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+  private $host = "localhost";
+  private $db_name = "blog-mvc";
+  private $username = "root";
+  private $password = "";
+  public $conn;
+
+  public function __construct() {
+    try {
+      $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+      $this->conn->exec("set names utf8");
+      echo "Connected successfully";
+    } catch (PDOException $e) {
+      echo "Connection error: " . $e->getMessage();
+    }
+  }
+}
